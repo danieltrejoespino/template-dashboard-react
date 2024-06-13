fe6f036cd0055098fedd9a7aed72ae5fa5166e19
Daniel Trejo <daniel.trejo.espino@gmail.com>
2024-06-12 23:55:38 -0600

    start with mui Grid

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,7 +2,23 @@ import { useState, useEffect, useRef, useContext } from 'react';
 import socketIOClient from 'socket.io-client';
 import { UserContext } from './UserContext';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+
 const ENDPOINT = "http://localhost:4000";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
 
 
 export const Chat = () => {
@@ -41,7 +57,30 @@ export const Chat = () => {
   };
 
   return <>
-      <p>Name: {user.name}</p>
+
+    <Box sx={{ flexGrow: 1 }} >
+      <Grid container spacing={2} >
+        {/* <Grid xs={12}>
+          <Item>
+            <Typography variant="h5" gutterBottom>
+              Chat
+            </Typography>
+          </Item>
+        </Grid> */}
+        <Grid xs={2}>
+          <Item>
+            <Typography variant="h5" gutterBottom>
+              Chats
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid xs={10}>
+          <Item>xs=9</Item>
+        </Grid>
+      </Grid>
+    </Box>
+
+    {/* <p>Name: {user.name}</p>
     <h1>Chat App</h1>
     <div>
       {messages.map((msg, index) => (
@@ -54,7 +93,7 @@ export const Chat = () => {
       value={message}
       onChange={handleMessage}
     />
-    <button onClick={sendMessage}>Send</button>
+    <button onClick={sendMessage}>Send</button> */}
 
   </>
 }
