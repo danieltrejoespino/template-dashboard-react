20caa0b3421d150027dc85590ef58d99f872c7bd
Daniel Trejo <daniel.trejo.espino@gmail.com>
2024-06-30 11:07:06 -0600

    test branch

--- src/components/Processes/GetRegister.jsx
+++ src/components/Processes/GetRegister.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+ 
 export default function GetRegister() {
   return (
     <div>GetRegister</div>
--- src/components/Reports/Test.jsx
+++ src/components/Reports/Test.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Test() {
   return (
-    <div>Test</div>
+    <div>Test1</div>
   )
 }
